52b4203a7aa09e38764cb72aab63678d15c4189a
jonpfluger <jonpfluger718@gmail.com>
2022-11-14 21:17:58 -0600

    search and add

--- index.js
+++ index.js
@@ -9,6 +9,56 @@ const connection = mysql.createConnection({
 })
 
 
+const searchActors = async () => {
+    const answer = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Last name: ',
+        }
+    ])
+
+    try {
+        const [results] = await connection.promise().query(`SELECT * FROM actor WHERE last_name = ?`, [answer.last_name])
+    } catch(err) {
+        throw new Error(err)
+    }
+
+
+
+    console.table(results)
+
+    menuPrompt()
+}
+
+const addActor = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'First name: '
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Last name: '
+        },
+    ])
+
+    try {
+        const [results] = await connection.promise().query(`INSERT INTO actor (first_name, last_name) VALUES (?, ?)`, [answers.first_name, answers.last_name])
+    } catch(err) {
+        throw new Error(err)
+    }
+
+    console.log('Actor added!')
+    menuPrompt()
+}
+
+const updateActor = () => {
+    
+}
+
 
 
 const menuPrompt = async () => {
