95cccb1b6ce044e78e19e4d642c270429fe95ec2
jonpfluger <jonpfluger718@gmail.com>
2022-11-14 21:41:13 -0600

    update actor

--- index.js
+++ index.js
@@ -20,15 +20,11 @@ const searchActors = async () => {
 
     try {
         const [results] = await connection.promise().query(`SELECT * FROM actor WHERE last_name = ?`, [answer.last_name])
+        console.log(results)
+        menuPrompt()
     } catch(err) {
         throw new Error(err)
     }
-
-
-
-    console.table(results)
-
-    menuPrompt()
 }
 
 const addActor = async () => {
@@ -55,10 +51,39 @@ const addActor = async () => {
     menuPrompt()
 }
 
-const updateActor = () => {
+const updateActor = async () => {
+    // what actor do you want to update? ID
+    const answers = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'actor_id',
+            message: 'Which actor do you want to update? (ID)'
+        },
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Update first name',
+            default: async (sessionAnswers) => {
+                const [results] = await connection.promise().query(`SELECT first_name FROM actor WHERE actor_id = ?`, sessionAnswers.actor_id)
+                return results[0].first_name
+            }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Update last name',
+            default: async (sessionAnswers) => {
+                const [results] = await connection.promise().query(`SELECT last_name FROM actor WHERE actor_id = ?`, sessionAnswers.actor_id)
+                return results[0].last_name
+            }
+        },
+    ])
     
-}
+    const [results] = await connection.promise().query(`UPDATE actor SET first_name = ?, last_name = ? WHERE actor_id = ?`, [answers.first_name, answers.last_name, answers.actor_id])
 
+    console.log("Actor updated!")
+    menuPrompt()
+}
 
 
 const menuPrompt = async () => {
@@ -84,4 +109,3 @@ const menuPrompt = async () => {
 }
 
 menuPrompt()
-
